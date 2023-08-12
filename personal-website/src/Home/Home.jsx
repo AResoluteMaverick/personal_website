b737b328a6d6b765de79e83460151a7500847075
AResoluteMaverick <official.raph.tapi@gmail.com>
2023-08-12 23:10:52 +0800

    Added birds

--- personal-website/src/Home/Home.jsx
+++ personal-website/src/Home/Home.jsx
@@ -4,6 +4,7 @@ import sky from '../assets/Parallax Assets/SKY.png';
 import sun from '../assets/Parallax Assets/SUN.png';
 import mountain from '../assets/Parallax Assets/MOUNTAIN.png';
 import forest from '../assets/Parallax Assets/FOREST.png';
+import birds from '../assets/Parallax Assets/image-from-rawpixel-id-6483181-svg.svg'
 import { useSpring, animated } from 'react-spring';
 import { useEffect, useState } from 'react';
 
@@ -45,6 +46,7 @@ function FirstSection () {
       const skyProps = useSpring({ transform: `translate3d(0, ${-scrollY * 0.1}px, 0)` });
       const sunProps = useSpring({ transform: `translate3d(0, ${scrollY * 0.4}px, 0)` });
       const mountainProps = useSpring({ transform: `translate3d(0, ${-scrollY * 0.25}px, 0)` });
+      const birdsProps = useSpring({ transform: `translate3d(${scrollY * 0.3}px, 0, 0) scale(0.5)` });
 
     return(
       <section className={styles.firstSection}>
@@ -52,6 +54,7 @@ function FirstSection () {
         <animated.img src={sun} className={styles.sun} style={sunProps} />
         <animated.img src={mountain} className={styles.mountain} style={mountainProps} />
         <animated.img src={forest} className={styles.forest} />
+        <animated.img src={birds} className={styles.birds} style={birdsProps}/>
       </section>
     )
 }
