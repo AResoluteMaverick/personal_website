0ee3f2272a112ef3d75c9338bf002bb3832d1e79
AResoluteMaverick <official.raph.tapi@gmail.com>
2023-08-12 19:35:58 +0800

    Download assets, and add to background

---
+++ personal-website/src/Header/Header.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+import styles from './Header.module.css';
+
+export default function Header () {
+    return (
+        <header className={styles.headerComponent}>
+            <Link to="/" className={styles.logo}>Logo</Link>
+            <ul className={styles.navlink}>
+                <li className={styles.liLink}><Link to="/">Home</Link></li>
+                <li className={styles.liLink}><Link to="/portfolio">Portfolio</Link></li>
+                <li className={styles.liLink}><Link to="/contact">Contact</Link></li>
+            </ul>
+        </header>
+    )
+}
--- personal-website/src/Home/Home.jsx
+++ personal-website/src/Home/Home.jsx
@@ -1,15 +1,26 @@
-import { Link } from "react-router-dom";
 import styles from './Home.module.css';
+import Header from '../Header/Header';
+import sky from '../assets/Parallax Assets/SKY.png';
+import sun from '../assets/Parallax Assets/SUN.png';
+import mountain from '../assets/Parallax Assets/MOUNTAIN.png';
+import forest from '../assets/Parallax Assets/FOREST.png';
+
+function FirstSection () {
+    return(
+        <section className={styles.firstSection}>
+            <img src={sky} className={styles.sky} />
+            <img src={sun} className={styles.sun} />
+            <img src={mountain} className={styles.mountain} />
+            <img src={forest} className={styles.forest} />
+        </section>       
+    )
+}
 
 export default function Home () {
     return (
-        <header>
-            <Link to="/" className={styles.logo}>Logo</Link>
-            <ul className={styles.navlink}>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/portfolio">Portfolio</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
-            </ul>
-        </header>
+        <>
+        <Header />
+        <FirstSection />
+        </>
     )
 }
