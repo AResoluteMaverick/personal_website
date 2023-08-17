28caba6975c36684a0682d80206def2fb43eb41e
AResoluteMaverick <official.raph.tapi@gmail.com>
2023-08-17 18:58:22 +0800

    Change flexibility of sections, and font of Header
    - Adjusted section ems to vw

--- personal-website/src/Header/Header.jsx
+++ personal-website/src/Header/Header.jsx
@@ -4,7 +4,7 @@ import styles from './Header.module.css';
 export default function Header () {
     return (
         <header className={styles.headerComponent}>
-            <Link to="/" className={styles.logo}>Logo</Link>
+            <Link to="/" className={styles.logo}>tapi</Link>
             <ul className={styles.navlink}>
                 <li className={styles.liLink}><Link to="/">Home</Link></li>
                 <li className={styles.liLink}><Link to="/portfolio">Portfolio</Link></li>
