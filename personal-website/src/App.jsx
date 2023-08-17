2c94a8af6f5516b967d3ef84ec2c3d0bf297d7c4
AResoluteMaverick <official.raph.tapi@gmail.com>
2023-08-17 13:42:12 +0800

    Createa a cursoir trailer

--- personal-website/src/App.jsx
+++ personal-website/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Home from './Home/Home.jsx'
+import CursorTrail from './assets/CursorTrail'
 
 function App() {
 
   return (
     <>
-      <Home />
+    <CursorTrail />
+    <Home />
     </>
   )
 }
---
+++ personal-website/src/assets/CursorTrail.jsx
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react';
+import { useSpring, animated } from 'react-spring';
+
+export default function CursorTrail () {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMouseMove = (event) => {
+      setPosition({ x: event.clientX, y: event.clientY });
+    };
+
+    window.addEventListener('mousemove', handleMouseMove);
+
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, []);
+
+  const springProps = useSpring({
+    left: position.x,
+    top: position.y,
+    from: { left: position.x, top: position.y },
+    config: {
+        tension: 300,
+        friction: 15,
+    }
+  });
+
+  return (
+    <animated.div
+      style={{
+        position: 'fixed',
+        width: '20px',
+        height: '20px',
+        background: 'red',
+        borderRadius: '50%',
+        pointerEvents: 'none',
+        transform: 'translate(-50%, -50%)',
+        zIndex: 9999,
+        ...springProps
+      }}
+    ></animated.div>
+  );
+}
