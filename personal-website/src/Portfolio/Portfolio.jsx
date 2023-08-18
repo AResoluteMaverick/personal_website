eec2fac65215fcc9dd93680dab282ea70354b970
AResoluteMaverick <official.raph.tapi@gmail.com>
2023-08-18 15:51:44 +0800

    Create layout for portfolio

--- personal-website/src/Portfolio/Portfolio.jsx
+++ personal-website/src/Portfolio/Portfolio.jsx
@@ -1,11 +1,27 @@
 import Header from "../Header/Header";
 import CursorTrail from "../assets/CursorTrail";
+import styles from './Portfolio.module.css';
+import portfolioData from '../assets/portfoliodescription.json';
 
 export default function Portfolio () {
+
     return (
         <>
         <CursorTrail />
         <Header />
+        <div className={styles.portfolioComponent}>
+            <div className={styles.portfolioNav}>
+                {
+                    portfolioData.map(item => (
+                        <h3 key={item.id}>{item.title}</h3>
+                    ))
+                }
+            </div>
+
+            <div className={styles.portfolioDisplay}>
+
+            </div>
+        </div>
         </>
     )
 }
