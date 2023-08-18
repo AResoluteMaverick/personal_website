e239e23cab372be5eb1b03e97d3696492c6a17f9
AResoluteMaverick <official.raph.tapi@gmail.com>
2023-08-18 20:48:49 +0800

    Stylize portfolioNavlink area
    - Add hover, focus, and font styling

--- personal-website/src/Portfolio/Portfolio.jsx
+++ personal-website/src/Portfolio/Portfolio.jsx
@@ -1,4 +1,5 @@
 import { Outlet, useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 import Header from "../Header/Header";
 import CursorTrail from "../assets/CursorTrail";
 import styles from './Portfolio.module.css';
@@ -6,7 +7,8 @@ import portfolioData from '../assets/portfoliodescription.json';
 
 export default function Portfolio () {
     const navigate = useNavigate();
-    
+    const [selectedItemId, setSelectedItemId] = useState(null);
+
     return (
         <>
         <CursorTrail />
@@ -16,9 +18,10 @@ export default function Portfolio () {
                 {portfolioData.map(item => (
                     <div 
                         key={item.id} 
-                        className={styles.portfolioItemTitle}
+                        className={`${styles.portfolioItemTitle} ${item.id === selectedItemId ? styles.selected : ''}`}
                         onClick={() => {
                             navigate(`project/${item.path}`, { state: { itemData: item } });
+                            setSelectedItemId(item.id);
                         }}
                     >
                         {item.title}
