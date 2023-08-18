4a5410630666530390e9d32234cbf93b2ca8395b
AResoluteMaverick <official.raph.tapi@gmail.com>
2023-08-18 23:25:04 +0800

    Create layout for contact

--- personal-website/src/Contact/Contact.jsx
+++ personal-website/src/Contact/Contact.jsx
@@ -1,12 +1,18 @@
 import Header from '../Header/Header';
+import CursorTrail from '../assets/CursorTrail';
+import MorphingSVG from '../assets/morphing/MorphingSVG';
 import styles from './Contact.module.css';
 
 export default function Contact () {
     return (
         <>
+        <CursorTrail />
         <Header />
+        <MorphingSVG />
         <div className={styles.contactComponent}>
-            Here is my contact!
+            <div className={styles.contactItem}>GitHub</div>
+            <div className={styles.contactItem}>official.raph.tapi@gmail.com</div>
+            <div className={styles.contactItem}>LinkedIn</div>
         </div>
         </>
     )
